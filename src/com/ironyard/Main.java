770a3c9e310c65be02ab05a80cf989e2b2ae02b4
DannyLocke <dlocke@mac.com>
2016-12-12 12:01:37 -0600

    Add Jar

--- src/com/ironyard/Main.java
+++ src/com/ironyard/Main.java
@@ -1,8 +1,22 @@
 package com.ironyard;
 
+import spark.Spark;
+import spark.template.mustache.MustacheTemplateEngine;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Spark.init();
+
+        Spark.get(
+                "/api/hello",
+                ((request, response) -> {
+
+                    return "Hello World!";
+                })
+                //new MustacheTemplateEngine()
+        );
+
     }
 }
