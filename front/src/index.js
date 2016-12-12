1bf5528fac55e5e8f02c4de4d03e443442069582
Sarah Rose Battles <srosebattles@gmail.com>
2016-12-12 16:46:17 -0600

    added NoMatch

--- front/src/index.js
+++ front/src/index.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, IndexRoute, Route, browserHistory} from 'react-router';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+/*  <Router history={browserHistory}>
+    <Route path="/" component={App} >,
+      <IndexRoute component={LandingPage} />
+      <Route path="/home" component={HomePage}/>
+      <Route path="/cars" component={CarsPage}>
+        <Route path="/car" component={SingleCar}/>
+      </Route>
+      <Route path="/cart" component={CartPage}/>
+      <Route path="/confirm" component={ConfirmPage}/>
+      <Route path="/success" component={SuccessPage}/>
+      <Route path="/battery" component={BatteryPage}/>
+
+      <Route path="*" component={NoMatch} />
+    </Route>
+
+  </Router>, */
+  
+  <App />
   document.getElementById('root')
 );
