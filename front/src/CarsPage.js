e5654895ec0a18b738c146e12ad1a8aa859e01a8
Logan Hussung <hussu072@umn.edu>
2016-12-13 17:56:28 -0600

    carspage

--- front/src/CarsPage.js
+++ front/src/CarsPage.js
@@ -1,9 +1,49 @@
 import React, { Component } from 'react';
+import api from './ApiCall.js';
+import axios from 'axios';
+
 
 export default class CarsPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      engine: '',
+      exteriorColor: '',
+      id: '',
+      image: '',
+      interiorColor: '',
+      model: '',
+      price: '',
+      year: '',
+      apiCall: api()
+    };
+  }
+
+  componentDidMount() {
+    this.getInitialData()
+  }
+
+  getInitialData() {
+    let inventory;
+    axios.get('/api/products').then((response) => {
+      inventory = response.data
+      this.setState({inventory})
+      console.log(inventory)
+    })
+  }
+
+
+
       render() {
         return (
-          <div>Cars
+          <div>
+            {/* <ul>
+              {this.state.inventory.map((car) => {
+                return (
+                  <li className='carEngine' key={car.id}>{car.engine}</li>
+                )
+              })}
+            </ul> */}
           </div>
         );
       }
--- front/src/index.js
+++ front/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, IndexRoute, Route, browserHistory} from 'react-router';
+import {Router, Route, browserHistory} from 'react-router';
 import App from './App';
 import './index.css';
 import LandingPage from './LandingPage';
