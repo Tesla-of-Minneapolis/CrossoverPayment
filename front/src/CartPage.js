11fe0fc4742d6fb99c8dcaeff91702abe08d9b21
Sarah Rose Battles <srosebattles@gmail.com>
2016-12-13 16:05:28 -0600

    homepage

--- front/src/CartPage.js
+++ front/src/CartPage.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 
 export default class CartPage extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      
+    }
+  }
       render() {
         return (
           <div>Cart
--- front/src/HomePage.js
+++ front/src/HomePage.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import dealership from './Images/homepagebuilding.jpg';
+import {Link} from 'react-router';
 import car1 from './Images/modelSbas.jpg';
 import car2 from './Images/modelXlux.jpg';
 import car3 from './Images/model3exo.jpg';
@@ -10,7 +11,7 @@ export default class HomePage extends Component {
           <div className="contentContainer">
             <img className="dealership" src={dealership} />
             <div>
-            <h2 className="red">Who We Are</h2>
+            <h2 className="red underline">Who We Are</h2>
             </div>
             <div className="hasText">
             <p>Tesla Minneapolis was dreamed up in 2015 by a group of engineers in Minneapolis who wanted to prove that electric cars could be better than gasoline-powered cars. With instant torque, incredible power, and zero emissions, Tesla’s products would be cars without compromise. Each new generation would be increasingly affordable, helping the company work towards its mission: to accelerate the world’s transition to sustainable transport.</p>
@@ -21,6 +22,10 @@ export default class HomePage extends Component {
             <img className="homeCar" src={car1} />
             <img className="homeCar" src={car2} />
             <img className="mobileCar" src={car3} />
+            </div>
+            <div className="hasText">
+            <p><h2 className="red adjustMargin"> Beauty. <br/> Power. <br/> Sustainability.</h2></p>
+            <p><Link className="homelink" to={"/cars"}>Cars</Link></p>
             </div>
           </div>
         );
