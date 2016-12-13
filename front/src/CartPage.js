7a8d7df227592a01926c4962bcfb296c2f472888
Sarah Rose Battles <srosebattles@gmail.com>
2016-12-13 17:01:41 -0600

    starting with dummy data

--- front/src/CartPage.js
+++ front/src/CartPage.js
@@ -4,7 +4,29 @@ export default class CartPage extends Component {
   constructor(props){
     super(props);
     this.state={
-      
+      items: [
+        {
+        id: 0,
+        img:'',
+        model: 'Model 3',
+        year: 2008,
+        battery: '60 kWh'
+
+      },
+      {
+        id: 0,
+        img:'',
+        model: 'Model S',
+        year: 2012,
+        battery: '60 kWh'
+      },
+      {
+        id: 0,
+        img:'',
+        model: 'Powerwall',
+        year: 2018,
+        battery: '60 kWh'
+      },],
     }
   }
       render() {
